35d6e3669b78c9e61f469ac6657c75cab4976f6d
Андрей Забавко <zeba.brat@Zebabrat.local>
2018-09-13 11:55:05 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.array = [];
+    this.comparator = (a,b) => a - b ;
   }
 
   add(element) {
     // your implementation
+    return this.array.push(element);
   }
 
   at(index) {
     // your implementation
+   return this.array[index];  
   }
 
   get length() {
     // your implementation
+    return this.array.length;
   }
 
   toArray() {
     // your implementation
+     return this.array
   }
 
   sort(indices) {
     // your implementation
+
+    let filterArr = this.array.filter((el, i) => indices.includes(i)).sort(this.comparator);
+
+    return this.array = this.array.map((el, i) => indices.includes(i) ? filterArr.shift() : el );
+    
   }
 
   setComparator(compareFunction) {
     // your implementation
+     return this.comparator = compareFunction;
   }
 }
 
